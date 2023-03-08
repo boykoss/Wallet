013455dcb4f94b2c50f482f0ea4ee62ee457809b
Zhanna <zhanna.vinogradova.1996@mail.ru>
2023-03-08 15:10:19 +0300

    lovi semen

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //semen_lovihui
-        //hjmhfmhc
+        //hjmhfmhc n nvnv n
     }
 }
